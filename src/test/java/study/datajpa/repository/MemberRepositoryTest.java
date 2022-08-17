28267956782a00e807004874c951a32117db9da6
iyj6707 <iyj6707@naver.com>
2022-08-17 22:56:26 +0900

    feat: 사용자 정의 리포지토리

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -234,4 +234,9 @@ class MemberRepositoryTest {
         // when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> list = memberRepository.findMemberCustom();
+    }
 }
